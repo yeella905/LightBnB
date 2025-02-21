c2bde40361afbc2cf6c54e3405ab50a27469b234
yeella905 <yeella905@gmail.com>
2025-02-20 19:48:43 -0500

    edit: changed the INSERT into data from the seeds.sql

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -15,6 +15,6 @@ VALUES ('2018-09-11', '2018-09-26', 2, 3),
 ('2021-10-01', '2021-10-14', 1, 3);
 
 INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
-VALUES (3, 2, 4, 3, 'message'),
-(2, 2, 5, 4, 'message'),
-(3, 1, 6, 4, 'message');
+VALUES (3, 2, 1, 3, 'message'),
+(2, 2, 2, 4, 'message'),
+(3, 1, 3, 4, 'message');
