85914e5af9e9d03a0a3d6c4b898f3bbaa20b0be6
yeella905 <yeella905@gmail.com>
2025-02-27 19:53:46 -0500

    edit: fix bugging issues for addProperty

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -195,7 +195,7 @@ const getAllProperties = function (options, limit) {
  */
 const addProperty = function (property) {
 
-  const queryString = "INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)"
+  const queryString = "INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *"
 
   const values = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code]
   
