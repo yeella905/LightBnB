fab048e2c201859d7d37a65d3328fd7bc081fafb
yeella905 <yeella905@gmail.com>
2025-02-27 19:49:26 -0500

    edit: refactor code for addProperty

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -69,8 +69,6 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
   
   const queryString = "INSERT INTO users (name, email, password) VALUES ($1, $2, $3)"
 
@@ -196,9 +194,7 @@ const getAllProperties = function (options, limit) {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  
+
   const queryString = "INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)"
 
   const values = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code]
